25c8c789ca7bec8b346441ca11e899c07923ba6b
Shashank <k.shashank.112@gmail.com>
2019-09-02 17:09:45 +0530

    Added JS functionality for personalized user message.

--- first-project/scripts/main.js
+++ first-project/scripts/main.js
@@ -3,10 +3,30 @@
 
 let myImage = document.querySelector('img');
 
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
 myImage.onclick = function() {
     if(myImage.getAttribute('src') === 'images/gotdog.jpg') {
         myImage.setAttribute('src', 'images/image2.jpg');
     } else{
         myImage.setAttribute('src', 'images/gotdog.jpg');
     }
+}
+
+function setUser(){
+    let myName = prompt("Please enter your name. ");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "This is a Husky, " + myName ;
+}
+
+if(!localStorage.getItem('name')){
+    setUser();
+} else{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = "This is a Husky, " + storedName ;
+}
+
+myButton.onclick = function(){
+    setUser();
 }
